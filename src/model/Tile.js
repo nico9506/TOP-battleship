f0eb902020e7a6f837ee3c8d2f203ea559dadba9
Nicolas Lopez <niclopezt@gmail.com>
2024-06-21 21:54:56 +1000

    feat: create class Tile
    Stores Ships (class instance) and a flag to know whether it has been hit
    or not

--- src/model/Tile.js
+++ src/model/Tile.js
@@ -1,9 +1,28 @@
 const Tile = class {
   /**
-   * 
+   * Data structure to represent a Tile of the Gameboard
+   * @param {Ship} ship - instance of Ship class to be saved in this tile
    */
 
-  constructor() {
+  #ship = null;
+  #isHit = false;
 
+  constructor(ship = null) {
+    this.#ship = ship;
+    this.#isHit = false;
   }
-}
+
+  // Getters and Setters
+  get ship() {
+    return this.#ship;
+  }
+
+  get isHit() {
+    return this.#isHit;
+  }
+
+  // Public methods
+  markTileasHit() {
+    this.#isHit = true;
+  }
+};
