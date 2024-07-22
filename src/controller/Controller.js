9c75fc2f65b8a802fad7919c23f5b5e99151ed7f
Nicolas Lopez <niclopezt@gmail.com>
2024-07-22 13:35:20 +1000

    refactor: Apply singleton pattern to Controller class

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -13,14 +13,14 @@ const Controller = class {
   // Player who is playing the current movement
   #currentPlayer = null;
 
-  constructor(
-    player1Name = "Player 1",
-    player1Human = true,
-    player2Name = "Player 2",
-    player2Human = false,
-  ) {
-    this.#player1 = new Player(player1Name, player1Human);
-    this.#player2 = new Player(player2Name, player2Human);
+  constructor() {
+    // Singleton pattern
+    if (Controller._instance) {
+      throw new Error(
+        "Class Controller can not be instantiated more than once",
+      );
+    }
+    Controller._instance = this;
   }
 
   // Getters and Setters
@@ -46,6 +46,23 @@ const Controller = class {
   checkGameOver() {
     return this.#player1.isWinner || this.#player2.isWinner ? true : false;
   }
+
+  restartInstance(
+    player1Name = "Player 1",
+    player1Human = true,
+    player2Name = "Player 2",
+    player2Human = false,
+  ) {
+    /**
+     * Set up new values for all attributes of the instance
+     */
+
+    this.#player1 = new Player(player1Name, player1Human);
+    this.#player2 = new Player(player2Name, player2Human);
+    this.#currentPlayer = null;
+  }
 };
 
-module.exports = Controller;
+const gameInstance = new Controller();
+
+exports.gameInstance = gameInstance;
